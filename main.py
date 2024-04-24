024e50a83ca23662461d507becf59d3d390ae1ba
AleBrambi <al3brambilla07@gmail.com>
2024-04-24 19:17:39 +0200

    setting base del main

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('prova')
+import pygame as x, sys
+import pygame.display
+from pygame.locals import *
+
+WINDOW_SIZE = (1000, 800)
+screen = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption('Home')
+
+clock = pygame.time.Clock()
+fps = 60
+
+while True:
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
