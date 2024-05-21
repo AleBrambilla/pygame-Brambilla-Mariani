8d6803da7ea8e51e4a20bd7bbcb54575be885ee4
AleBrambi <al3brambilla07@gmail.com>
2024-05-21 20:55:35 +0200

    abbozzo piattaforme mobili

--- piattaforme.py
+++ piattaforme.py
@@ -19,19 +19,59 @@ class Piattaforma(pygame.sprite.Sprite):
             self.rect.y+=3
             if self.rect.y>805:
                 self.kill()
+            
+class Classica(Piattaforma):
+    def __init__(self, y):
+        super().__init__(y)
+    def update() -> None:
+        super().scorri()
+
+class Mobile_x(Piattaforma):
+    def __init__(self, y):
+        super().__init__(y)
+        self.delta=randint(50,200)
+        self.centro=self.rect.centerx
+        self.direzione=-1
+    
+    def update(self):
+        super().scorri()
+        self.rect.x+=self.direzione*3
+        if self.rect.x < self.centro - self.delta: self.direzione = 1
+        if self.rect.x > self.centro + self.delta: self.direzione = -1
+
+class Mobile_y(Piattaforma):
+    def __init__(self, y):
+        super().__init__(y)
+        self.delta=randint(25,100)
+        self.centro=self.rect.centery
+        self.direzione=-1
+    
+    def update(self):
+        super().scorri()
+        self.rect.y+=self.direzione*3
+        if self.rect.y < self.centro - self.delta: self.direzione = 1
+        if self.rect.y > self.centro + self.delta: self.direzione = -1
+
+
+
+
 
 def corrente(piattaforme, player):          
     for i,piattaforma in enumerate(piattaforme.sprites()):
-                if not piattaforma.corrente:    
+                
+                if not piattaforma.corrente:  
                     if piattaforma.rect.colliderect(player.sprite.rect):
+
                         piattaforme.sprites()[i].corrente=True
-                        #print(piattaforma.corrente)
+
                         for j in range(len(piattaforme.sprites()[:i-1:])):
                             piattaforme.sprites()[j].corrente=False  
         
 def bool_scorrere(piattaforme, player):
-    scorri=True
+
     corrente(piattaforme, player)
+
+    scorri=True
     for piattaforma in piattaforme.sprites(): 
         if piattaforma.rect.top >=700 and piattaforma.corrente:
             scorri=False
